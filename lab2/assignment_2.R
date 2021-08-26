6ad7b18697a9c6d47d9783b9ba35b1a8effc09da
jyotikurmi <77629696+jyotikurmi@users.noreply.github.com>
2021-08-26 22:35:44 +0530

    Update assignment_2.R

--- lab2/assignment_2.R
+++ lab2/assignment_2.R
@@ -1,1 +1,21 @@
 
+a <- read.csv("Dataset.csv",header = TRUE ,sep = ",")
+
+data3 <- a
+data3$Age[is.na(data3$Age)]<-median(data3$Age, na.rm=TRUE)
+data3$Salary[is.na(data3$Salary)]<-median(data3$Salary, na.rm=TRUE)
+
+
+data3$Country = factor(data3$Country ,
+levels = c('France','Spain','Germany'),
+labels = c(1,2,3))
+
+data3$Purchased = factor(data3$Purchased ,
+                       levels = c('Yes','No'),
+                       labels = c(0,1))
+View(data3)
+
+data3$Country <- as.numeric(as.factor(data3$Country))
+data3$Purchased <- as.numeric(as.factor(data3$Purchased))
+data3 <- scale(data3, center = TRUE, scale = TRUE)
+View(data3)
