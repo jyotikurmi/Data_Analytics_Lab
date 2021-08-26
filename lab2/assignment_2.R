f3473b58747f95aea4f5e0ea866d9add15280398
jyotikurmi <77629696+jyotikurmi@users.noreply.github.com>
2021-08-26 22:39:28 +0530

    Update assignment_2.R

--- lab2/assignment_2.R
+++ lab2/assignment_2.R
@@ -1,10 +1,14 @@
-
+# question no. 1
+# loading the dataset in r
 a <- read.csv("Dataset.csv",header = TRUE ,sep = ",")
 
 data3 <- a
+
+#replace na values in a particular column with the median of that column
 data3$Age[is.na(data3$Age)]<-median(data3$Age, na.rm=TRUE)
 data3$Salary[is.na(data3$Salary)]<-median(data3$Salary, na.rm=TRUE)
 
+# encoding the categorical data
 
 data3$Country = factor(data3$Country ,
 levels = c('France','Spain','Germany'),
@@ -15,6 +19,7 @@ data3$Purchased = factor(data3$Purchased ,
                        labels = c(0,1))
 View(data3)
 
+#scaling the dataset
 data3$Country <- as.numeric(as.factor(data3$Country))
 data3$Purchased <- as.numeric(as.factor(data3$Purchased))
 data3 <- scale(data3, center = TRUE, scale = TRUE)
