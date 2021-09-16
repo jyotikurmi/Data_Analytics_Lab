73f7153bb8dd99736a01a9f64f66511e274296f2
jyotikurmi <77629696+jyotikurmi@users.noreply.github.com>
2021-09-16 22:42:54 +0530

    Create 1_3.R

---
+++ lab3/1_3.R
@@ -0,0 +1,5 @@
+data <- matrix(rnorm(100, 0, 10), nrow = 10, ncol = 10)           
+colnames(data) <- paste0("col", 1:10)                             
+rownames(data) <- paste0("row", 1:10)   
+View (data)
+heatmap(data, Rowv = NA, Colv = NA)
